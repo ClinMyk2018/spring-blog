60e34a2a2c361f8aea2e459f41c1959839a020cf
Mykal Clinard <mykalclinard@gmail.com>
2019-06-26 11:20:00 -0500

    first exercise complete

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,22 +1,39 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import static org.codehaus.groovy.runtime.StringGroovyMethods.reverse;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    @ResponseBody
-    public String hello() {
-        return "This will be the landing page!";
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String hello() {
+//        return "This will be the landing page!";
+//    }
+
+//    @GetMapping("/admin/{name}")
+//    @ResponseBody
+//    public String helloAdmin(@PathVariable String name){
+//        return "Hello admin, " + name;
+//    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "Home";
     }
 
-    @GetMapping("/admin/{name}")
+    @GetMapping("/cooltrick/{word}")
     @ResponseBody
-    public String helloAdmin(@PathVariable String name){
-        return "Hello admin, " + name;
-    }
+    public String cooltrick(@PathVariable String word){
+//        return word.toUpperCase();
+//         return String.valueOf(word.toUpperCase().charAt(3));
+         return reverse(word).toUpperCase();
 
+    }
 
 }
